8d280e3abf2b57ef186a12f32b1128c152107537
vc1033 <vaishnavc9745@gmail.com>
2023-07-23 18:58:51 +0530

    Price Cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const PriceCartApp = () => {
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: "Item 1", price: 20, quantity: 1 },
+    { id: 2, name: "Item 2", price: 30, quantity: 1 },
+    { id: 3, name: "Item 3", price: 15, quantity: 1 },
+  ]);
+
+  const increaseQuantity = (itemId) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (itemId) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      )
+    );
+  };
+
+  const calculateTotal = () => {
+    const total = cartItems.reduce(
+      (accumulator, item) => accumulator + item.price * item.quantity,
+      0
+    );
+    return total;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h2 className="text-center mb-4">Price Cart</h2>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>
+                <button
+                  className="btn btn-sm btn-primary mr-2"
+                  onClick={() => decreaseQuantity(item.id)}
+                >
+                  -
+                </button>
+                {item.quantity}
+                <button
+                  className="btn btn-sm btn-primary ml-2"
+                  onClick={() => increaseQuantity(item.id)}
+                >
+                  +
+                </button>
+              </td>
+              <td>${item.price.toFixed(2)}</td>
+              <td>${(item.price * item.quantity).toFixed(2)}</td>
+            </tr>
+          ))}
+          <tr>
+            <td colSpan="3" className="text-right font-weight-bold">
+              Total
+            </td>
+            <td>${calculateTotal().toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default PriceCartApp;
